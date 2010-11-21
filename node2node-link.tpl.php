14badb96317785a13ef4072174f3f9e7fc0b5f2c
Siegfried Dünkel <chaosprinz76@googlemail.com>
2010-11-21 11:47:17 +0100

    implementation of preprocess-function & tpl-file: NEEDS FIX: tpl.php doesnt run. cant see the error in the moment.

---
+++ node2node-link.tpl.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Theming the outputted link created in hook_form_alter
+ */
+?>
+
+<div class="node2node-button">
+  <a href="<?php print $node2node['href'];?>" class="node2node-link">
+    <?php print $node2node['title'] . 'möppelpöppeldöppeldu';?>
+  </a>
+
+  <h1>haaaalloooo</h1>
+</div>
